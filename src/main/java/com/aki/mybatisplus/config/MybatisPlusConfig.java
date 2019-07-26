18f36e769521267b348aaee75da86737a2937dc8
kamiaki <247557768@qq.com>
2019-07-26 11:30:57 +0800

    更新

--- src/main/java/com/aki/mybatisplus/config/MybatisPlusConfig.java
+++ src/main/java/com/aki/mybatisplus/config/MybatisPlusConfig.java
@@ -1,6 +1,8 @@
 package com.aki.mybatisplus.config;
 
+import com.baomidou.mybatisplus.extension.plugins.OptimisticLockerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,12 +13,38 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan("com.aki.mybatisplus.mapper")
 public class MybatisPlusConfig {
 
+    /***
+     * plus 的性能优化
+     * @return
+     */
+    @Bean
+    public PerformanceInterceptor performanceInterceptor() {
+        PerformanceInterceptor performanceInterceptor = new PerformanceInterceptor();
+        /*<!-- SQL 执行性能分析，开发环境使用，线上不推荐。 maxTime 指的是 sql 最大执行时长 -->*/
+        performanceInterceptor.setMaxTime(1000);
+        /*<!--SQL是否格式化 默认false-->*/
+        performanceInterceptor.setFormat(true);
+        return performanceInterceptor;
+    }
+
+
     /**
-     * 分页插件
+     * mybatis-plus分页插件
      */
     @Bean
     public PaginationInterceptor paginationInterceptor() {
-        return new PaginationInterceptor();
+        PaginationInterceptor page = new PaginationInterceptor();
+        page.setDialectType("mysql");
+        return page;
+    }
+
+    /**
+     * mybatis-plus乐观锁插件
+     */
+    @Bean
+    public OptimisticLockerInterceptor optimisticLockerInterceptor() {
+        OptimisticLockerInterceptor oLocker = new OptimisticLockerInterceptor();
+        return oLocker;
     }
 
 }
